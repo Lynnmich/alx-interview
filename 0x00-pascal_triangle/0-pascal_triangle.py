4a6e064d78493bf461a6b4d79c19c01a939f1956
Lynnmich <lynnenmichuki@gmail.com>
2023-07-25 16:55:10 +0000

    Modified the code

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,18 +1,16 @@
+#!/usr/bin/python3
+"""
+Print Pascal's Triangle
+"""
+
+
 def pascal_triangle(n):
-    if n <= 0:
-        return []
-
-    triangle = [[1]]  # the first row
-
-    for row in range(1, n):
-        current_row = [1]  # the first element of each row is 1
-
-        for position in range(1, len(triangle[row-1])):
-            value = triangle[row-1][position] + triangle[row-1][position-1]
-            current_row.append(value)
-
-        current_row.append(1)  # the last element of each row is 1
-        triangle.append(current_row)
-
-    return triangle
-~                        
+    """Returns Pascal's triangle in a list of a list"""
+    my_list = []
+    if (n <= 0):
+        return my_list
+    my_list.append([1])
+    for i in range(n - 1):
+        my_list.append([1] + [my_list[i][j] + my_list[i][j + 1]
+                              for j in range(len(my_list[i]) - 1)] + [1])
+    return my_list
