704ddb6fa3429dd02e22db009de22a8b32f98ec5
Lynnmich <lynnenmichuki@gmail.com>
2023-07-25 03:37:12 +0000

    PascalS Triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,20 @@
+def pascal_triangle(n):
+    triangle = []
+
+    if n <= 0:
+        return triangle
+
+    triangle.append([1])  # the first row
+
+    for row in range(1, n):
+        current_row = [1]  # the first element of each row is 1
+
+        for position in range(1, len(triangle[row-1])):
+            value = triangle[row-1][position] + triangle[row-1][position-1]
+            current_row.append(value)
+
+        current_row.append(1)  # the last element of each row is 1
+        triangle.append(current_row)
+
+    return triangle
+~                        
