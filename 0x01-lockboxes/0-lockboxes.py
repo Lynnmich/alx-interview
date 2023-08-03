703ae7c58d64a5e9a78e2220f8c84b97b23a5ee8
Lynnmich <lynnenmichuki@gmail.com>
2023-08-03 16:40:44 +0300

    Corrected code

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -3,16 +3,13 @@
 
 
 def canUnlockAll(boxes):
-    n = len(boxes)
-    unlocked = [False] * n
-    stack = [0]  
-
-    while stack:
-        box = stack.pop()
-        unlocked[box] = True
-
-        for key in boxes[box]:
-            if not unlocked[key]:
-                stack.append(key)
-
-    return all(unlocked)
+    unlocked = [0]
+    for box_id, box in enumerate(boxes):
+        if not box:
+            continue
+        for key in box:
+            if key < len(boxes) and key not in unlocked and key != box_id:
+                unlocked.append(key)
+    if len(unlocked) == len(boxes):
+        return True
+    return False
