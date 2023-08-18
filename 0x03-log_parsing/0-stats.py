36c89aac4b669a9047c96d19f941ae5f733be91f
Lynnmich <lynnenmichuki@gmail.com>
2023-08-18 06:36:28 +0300

    Task

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,43 +1,37 @@
-#!/usr/bin/python3
+#!/usr/bin/python
 """A script that reads stdin line by line and computes metrics"""
+
 
 import sys
 
+cache = {'200': 0, '301': 0, '400': 0, '401': 0,
+         '403': 0, '404': 0, '405': 0, '500': 0}
+total_size = 0
+counter = 0
 
-def print(total_size, cache):
-    """Prints total file size and cache list"""
-    print("File size: {:d}".format(total_size))
+
+def print_n(total_size):
+    """Prints the statistics"""
+    print("file size: {}".format(total_size))
     for key, value in sorted(cache.items()):
         if value != 0:
             print("{}: {}".format(key, value))
 
 
-cache = {'200': 0, '301': 0, '400': 0, '401': 0,
-         '403': 0, '404': 0, '405': 0, '500': 0}
-
-total_size = 0
-counter = 0
-
 try:
     for line in sys.stdin:
-        line_list = line.split()
-
-        if len(line_list) > 2:
-            code = line_list[-2]
-            size = int(line_list[-1])
-
+        rline = line.split(" ")
+        if len(rline) > 4:
+            code = rline[-2]
             if code in cache.keys():
                 cache[code] += 1
-
-            total_size += size
+            filesize = int(rline[-1])
+            total_size += filesize
             counter += 1
-
         if counter == 10:
-            print('File size: {}'.format(total_size))
             counter = 0
-
-except Exception as err:
+            print_n(total_size)
+except Exception as ex:
     pass
-
 finally:
-    print('File size: {}'.format(total_size))
+    print_n(total_size)
