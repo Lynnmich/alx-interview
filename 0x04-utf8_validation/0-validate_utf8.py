cfafc669d4b410c8882fd8082a916ca1597b20e0
Lynnmich <lynnenmichuki@gmail.com>
2023-08-23 15:11:41 +0000

    Task 5

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -7,22 +7,15 @@ def validUTTF8(data):
     remaining_bytes = 0
     """iterate thro' each integer in the list"""
     for byte in data:
-        binary_representation = .format(byte, '08b')
-        if remaining_bytes == 0:
-            """count the number of leading '1' bits"""
-            for bit in binary_represantation:
-                if bit == '0':
-                    break
-                remaining_bytes += 1
-                if remaining_bytes == 0:
-                    continue
-                """characters can have 1 to 4 bytes"""
-                if remaining_bytes < 2 or remaining_bytes > 4:
-                    return False
-        else:
-            """check if current byte starts with '10'"""
-            if not(binary_representation[0] == '1'
-                   and binary_representation[1] == '0'):
+        value = byte & 255
+        if remaining_bytes:
+            if value >> 6 != 2:
                 return False
             remaining_bytes -= 1
+            continue
+        while (1 << abs(7 - remaining_bytes)) & value:
+            remaining_bytes += 1
+        if remaining_bytes == 1 or remaining_bytes > 4:
+            return False
+        remaining_bytes = max(remaining_bytes - 1, 0)
     return remaining_bytes == 0
