0e450b226aacefbee9d7238dd94a6ab879233df9
Lynnmich <lynnenmichuki@gmail.com>
2023-09-08 09:46:19 +0300

    Task 0

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,20 +1,19 @@
 #!/usr/bin/node
+
 const util = require('util');
-const request = util.promisify(require('request'));
-const filmID = process.argv[2];
+const request = require('request');
 
-async function starwarsCharacters (filmId) {
-  const endpoint = 'https://swapi-api.hbtn.io/api/films/' + filmId;
-  let response = await (await request(endpoint)).body;
-  response = JSON.parse(response);
-  const characters = response.characters;
-
-  for (let i = 0; i < characters.length; i++) {
-    const urlCharacter = characters[i];
-    let character = await (await request(urlCharacter)).body;
-    character = JSON.parse(character);
-    console.log(character.name);
-  }
+if (process.argv[2] === undefined) {
+  console.log('Usage: node <FILENAME> <id>');
+} else {
+  (async () => {
+    const requestPromise = util.promisify(request);
+    const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+    const body = (await requestPromise(url)).body;
+    const abc = JSON.parse(body).characters;
+    for (let i = 0; i < abc.length; i++) {
+      const char = (await requestPromise(abc[i])).body;
+      console.log(JSON.parse(char).name);
+    }
+  })();
 }
-
-starwarsCharacters(filmID);
